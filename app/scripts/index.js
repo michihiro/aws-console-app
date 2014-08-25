82e98b452dd8b05470f5119e7e458d62234330b4
michihiro <muta.michihiro@gmail.com>
2014-08-25 12:52:38 +0900

    view を追加

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,8 +1,46 @@
 'use strict';
 
 var ng = angular;
-ng.module('aws-console', [])
-  .controller('mainCtrl', mainCtrl);
+ng.module('aws-console', [
+    'ui.router'
+  ])
+  .config(appConfig)
+  .run(appRun);
 
-function mainCtrl() {
+appConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function appConfig($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url:'/',
+      templateUrl: 'views/home.html',
+      controller: 'homeCtrl'
+    })
+    .state('s3', {
+      params: ['bucket'],
+      templateUrl: 'views/s3.html',
+      controller: 's3Ctrl'
+    });
 }
+
+
+appRun.$inject = ['$rootScope', '$state', '$stateParams' ];
+function appRun($rootScope, $state, $stateParams) {
+  $rootScope.$state = $state;
+  $rootScope.state = $state;
+  $rootScope.$stateParams = $stateParams;
+}
+
+
+homeCtrl.$inject = ['$scope'];
+function homeCtrl() {
+}
+
+
+s3Ctrl.$inject = ['$scope', '$state', '$stateParams' ];
+function s3Ctrl($scope, $state, $stateParams) {
+  console.log($state, $stateParams);
+}
+
