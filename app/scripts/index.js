5fb861c7daa94d6b44aa78c9f73dc08396cd8a99
michihiro <muta.michihiro@gmail.com>
2014-08-31 14:54:22 +0900

    ダイアログ追加

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,7 +2,9 @@
 
 var ng = angular;
 ng.module('aws-console', [
-    'ui.router'
+    'ui.router',
+    'ui.utils',
+    'ui.bootstrap'
   ])
   .config(appConfig)
   .run(appRun);
@@ -26,11 +28,27 @@ function appConfig($stateProvider, $urlRouterProvider) {
 }
 
 
-appRun.$inject = ['$rootScope', '$state', '$stateParams' ];
-function appRun($rootScope, $state, $stateParams) {
+appRun.$inject = ['$rootScope', '$state', '$stateParams','$modal' ];
+function appRun($rootScope, $state, $stateParams, $modal) {
   $rootScope.$state = $state;
   $rootScope.state = $state;
   $rootScope.$stateParams = $stateParams;
+
+
+  $rootScope.openDialog = function(tpl, resolve) {
+    var modalInstance = $modal.open({
+      templateUrl: 'views/' + tpl,
+//      controller: ModalInstanceCtrl,
+//      size: size,
+      resolve: resolve
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $rootScope.selected = selectedItem;
+    }, function () {
+      console.log('Modal dismissed at: ' + new Date());
+    });
+  };
 }
 
 
@@ -42,5 +60,18 @@ function homeCtrl() {
 s3Ctrl.$inject = ['$scope', '$state', '$stateParams' ];
 function s3Ctrl($scope, $state, $stateParams) {
   console.log($state, $stateParams);
+}
+
+dialogCredentialsCtrl.$inject = ['$scope', '$timeout'];
+function dialogCredentialsCtrl($scope, $timeout) {
+  $scope.save = function() {
+    $scope.processing = true;
+
+    $timeout(function() {
+      $scope.processing = false;
+      $scope.$close();
+    }, 3000);
+  };
+
 }
 
