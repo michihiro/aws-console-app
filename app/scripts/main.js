6e2c1298e26e969a411cbc26591a234f2af0a4b8
michihiro <muta.michihiro@gmail.com>
2014-09-05 20:00:27 +0900

    初期サイズ変更

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,14 +2,16 @@
 
 // Listens for the app launching then creates the window
 chrome.app.runtime.onLaunched.addListener(function() {
-  var width = 500;
-  var height = 300;
+  var width = 960;
+  var height = 640;
 
   chrome.app.window.create('index.html', {
     id: 'main',
-    bounds: {
+    innerBounds: {
       width: width,
       height: height,
+      minWidth: 480,
+      minHeight: 320,
       left: Math.round((screen.availWidth - width) / 2),
       top: Math.round((screen.availHeight - height)/2)
     }
