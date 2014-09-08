d2e62a106bb66a61973ebae59dc25cd06e3f0702
michihiro <muta.michihiro@gmail.com>
2014-09-09 06:59:02 +0900

    通知仮実装

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,3 +17,81 @@ chrome.app.runtime.onLaunched.addListener(function() {
     }
   });
 });
+
+var feeds = [
+  {
+    url: 'http://status.aws.amazon.com/rss/ec2-us-west-1.rss'
+  }
+];
+feeds.forEach(function(v) {
+  v.lastPublished = Date.now();
+});
+
+chrome.alarms.create('status-check', {
+  periodInMinutes: 1
+});
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  if (!alarm) {
+    return;
+  }
+  if (alarm.name === 'status-check') {
+    statusCheck(feeds[0]);
+  }
+});
+//setTimeout(function() {statusCheck(feeds[0]);}, 1000);
+
+function statusCheck(feed) {
+  var url = 'https://ajax.googleapis.com/ajax/services/feed/load';
+  var xhr = new XMLHttpRequest();
+  var opts = {
+    v: '1.0',
+    output: 'json',
+    q: feed.url
+  };
+  url += '?' + Object.keys(opts).map(function(k) {
+    return k + '=' + encodeURIComponent(opts[k]);
+  }).join('&');
+
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = function() {
+    console.log('statechange', arguments);
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response;
+      try {
+        response = JSON.parse(xhr.response);
+      } catch (e) {}
+
+      //console.log(xhr.response);
+      if (!response ||
+        !response.responseData ||
+        !response.responseData.feed.entries ||
+        !response.responseData.feed.entries.length) {
+        return;
+      }
+
+      var entries = response.responseData.feed.entries;
+
+      entries.forEach(function(v) {
+        var publishedDate = new Date(v.publishedDate);
+        if (publishedDate.getTime() <= feed.lastPublished) {
+          return;
+        }
+        feed.lastPublished = publishedDate.getTime();
+        var opt = {
+          type: 'basic',
+          title: 'From Service Health Dashboard',
+          message: v.title,
+          contextMessage: v.contentSnippet,
+          iconUrl: 'images/icon-128.png',
+          isClickable: true
+        };
+        var id = feed + publishedDate;
+        chrome.notifications.create(id, opt, function() {
+          console.log('callback');
+        });
+      });
+
+    }
+  };
+  xhr.send(opts);
+}
