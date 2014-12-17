a89d790c730317ed416d56a1520d3d019d932b93
michihiro <muta.michihiro@gmail.com>
2014-12-17 19:58:54 +0900

    パスワード設定対応

--- app/js/common/controller.js
+++ app/js/common/controller.js
@@ -3,7 +3,9 @@
 
   ng.module('aws-console')
     .service('credentialsService', credentialsService)
-    .controller('comCredentialsDialogCtrl', comCredentialsDialogCtrl);
+    .controller('comCredentialsDialogCtrl', comCredentialsDialogCtrl)
+    .factory('passwordService', passwordServiceFactory)
+    .controller('comPasswordDialogCtrl', comPasswordDialogCtrl);
 
   comCredentialsDialogCtrl.$inject = ['$scope', '$timeout', '$filter', 'credentialsService'];
 
@@ -58,12 +60,13 @@
     }
   }
 
-  credentialsService.$inject = ['$rootScope', '$q', '$timeout', '$interval'];
+  credentialsService.$inject = ['$rootScope', '$q', '$timeout', '$interval', 'passwordService'];
 
-  function credentialsService($rootScope, $q, $timeout, $interval) {
+  function credentialsService($rootScope, $q, $timeout, $interval, passwordService) {
     var storage = chrome.storage.local;
     var credentials;
     var temporaryCredentials;
+    var intervalPromise;
 
     return {
       getCredentials: getCredentials,
@@ -78,6 +81,35 @@
     function load(flagUpdate) {
       var deferred = $q.defer();
 
+      storage.get('encCredentials', function(val) {
+        if (!val || !val.encCredentials) {
+          deferred.reject({});
+          return;
+        }
+
+        passwordService.decryptCredentials(val.encCredentials)
+          .then(function(decCredentials) {
+            decCredentials = JSON.parse(decCredentials);
+
+            if (!flagUpdate) {
+              deferred.resolve(decCredentials);
+              return;
+            }
+
+            credentials = decCredentials;
+
+            refreshCredentials(credentials.accessKeyId)
+              .then(function(id) {
+                $rootScope.credentialsId = id;
+                deferred.resolve(val.credentials);
+                if(intervalPromise) {
+                  $interval.cancel(intervalPromise);
+                }
+                intervalPromise = $interval(refreshCredentials, 600000);
+              });
+          });
+      });
+/*
       storage.get('credentials', function(val) {
         if (!val || !val.credentials) {
           deferred.reject({});
@@ -97,11 +129,21 @@
           });
         });
       });
+*/
       return deferred.promise;
     }
 
     function save(credentials) {
       var deferred = $q.defer();
+
+      passwordService.encryptCredentials(JSON.stringify(credentials))
+        .then(function(data) {
+          chrome.storage.local.set({
+            encCredentials: data,
+          }, function() {
+            deferred.resolve();
+          });
+        });
 
       chrome.storage.local.set({
         credentials: credentials
@@ -112,7 +154,8 @@
       return deferred.promise;
     }
 
-    function refreshCredentials(credId, cb) {
+    function refreshCredentials(credId) {
+      var deferred = $q.defer();
       AWS.config.credentials = new AWS.Credentials(credentials);
       var tempCred = new AWS.TemporaryCredentials({
         DurationSeconds: 900,
@@ -121,12 +164,145 @@
         temporaryCredentials = tempCred;
         AWS.config.credentials = null;
         temporaryCredentials.masterCredentials = null;
-        if(cb) {
-          cb(credId);
-        }
+        deferred.resolve(credId);
       });
+      return deferred.promise;
     }
 
+  }
+
+  comPasswordDialogCtrl.$inject = ['$scope', 'passwordService', 'dialogInputs'];
+
+  function comPasswordDialogCtrl($scope, passwordService, dialogInputs) {
+    var storage = chrome.storage.local;
+
+    ng.extend($scope, {
+      mode: dialogInputs.mode,
+      inputs: {},
+      auth: auth,
+      setPassword: setPassword,
+    });
+
+    function auth() {
+      $scope.processing = true;
+      passwordService.auth($scope.inputs.password)
+        .then(function() {
+          $scope.$close($scope.inputs.password);
+        })
+        .finally(function() {
+          $scope.processing = false;
+        });
+    }
+
+    function setPassword() {
+      var hash = CryptoJS.SHA256($scope.inputs.password + chrome.runtime.id)
+                 .toString(CryptoJS.enc.Base64);
+      storage.set({passwordHash: hash}, function() {
+        $scope.$close($scope.inputs.password);
+      });
+    }
+  }
+
+  passwordServiceFactory.$inject = ['$rootScope', '$q'];
+
+  function passwordServiceFactory($rootScope, $q) {
+    var storage = chrome.storage.local;
+    var password;
+
+    var jsonFormatter = {
+      stringify: function (cipherParams) {
+        var jsonObj = {
+          ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64),
+        };
+
+        if (cipherParams.iv) {
+          jsonObj.iv = cipherParams.iv.toString();
+        }
+        if (cipherParams.salt) {
+          jsonObj.s = cipherParams.salt.toString();
+        }
+
+        return JSON.stringify(jsonObj);
+      },
+
+      parse: function (jsonStr) {
+        var jsonObj = JSON.parse(jsonStr);
+
+        var cipherParams = CryptoJS.lib.CipherParams.create({
+          ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct)
+        });
+
+        if (jsonObj.iv) {
+          cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv);
+        }
+        if (jsonObj.s) {
+          cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s);
+        }
+
+        return cipherParams;
+      }
+    };
+
+    return {
+      encryptCredentials: encryptCredentials,
+      decryptCredentials: decryptCredentials,
+      auth: auth,
+    };
+
+    function decryptCredentials(inData) {
+      var deferred = $q.defer();
+      var promise, dlg;
+
+      if(password === undefined) {
+        dlg = $rootScope.openDialog('com/passwordDialog', {mode:'auth'});
+        promise = dlg.result;
+      } else {
+        promise = $q.when(password);
+      }
+
+      promise.then(function(password) {
+        var keyStr = password + chrome.runtime.id;
+        var decData = CryptoJS.AES.decrypt(inData, keyStr, {format: jsonFormatter });
+        deferred.resolve(decData.toString(CryptoJS.enc.Utf8));
+      });
+
+      return deferred.promise;
+    }
+
+    function encryptCredentials(inData) {
+      var deferred = $q.defer();
+      var promise, dlg;
+
+      if(password === undefined) {
+        dlg = $rootScope.openDialog('com/passwordDialog', {mode:'set'});
+        promise = dlg.result;
+      } else {
+        promise = $q.when(password);
+      }
+
+      promise.then(function(password) {
+        var keyStr = password + chrome.runtime.id;
+        var encData = CryptoJS.AES.encrypt(inData, keyStr, {format: jsonFormatter})
+          .toString();
+        deferred.resolve(encData);
+      });
+
+      return deferred.promise;
+    }
+
+    function auth(inPassword) {
+      var deferred = $q.defer();
+      var hash = CryptoJS.SHA256(inPassword + chrome.runtime.id).toString(CryptoJS.enc.Base64);
+      storage.get('passwordHash', function(val) {
+        if(val.passwordHash === hash) {
+          password = inPassword;
+          deferred.resolve();
+        } else {
+          deferred.reject();
+        }
+      });
+      return deferred.promise;
+    }
   }
 
 })(angular);
